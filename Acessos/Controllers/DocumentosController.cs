fd24db86b10c1bd29140b97c2eafea3acefd1049
Fábio Nunes <fabionunesbr@gmail.com>
2024-05-20 07:48:52 -0300

    feat: criados paths da API documentos

---
+++ Acessos/Controllers/DocumentosController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Acessos.Controllers;
+
+[ApiController]
+[Route("api/v1/documentos")]
+public class DocumentosController : ControllerBase
+{
+    /// <summary>
+    /// Retorna coleção com todos os documentos cadastrados.
+    /// </summary>
+    /// <response code="200">Retorna os documentos com sucesso</response>
+    /// <response code="500">Se houver um erro interno no servidor</response>
+    [HttpGet ("todos")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public IActionResult RetornarDocumentos()
+    {
+        throw new ApplicationException("Método não concluido");
+    }
+
+    /// <summary>
+    /// Retorna um documento específico por ID.
+    /// </summary>
+    /// <param name="documentoId">Id do documento</param>
+    /// <response code="200">Retorna o documento com sucesso</response>
+    /// <response code="500">Se houver um erro interno no servidor</response>
+    [HttpGet ("{documentoId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public IActionResult Todos(int id)
+    {
+        throw new ApplicationException("Método não concluido");
+    }
+
+
+    /// <summary>
+    /// Inseri um novo documento.
+    /// </summary>
+    /// <response code="200">Retorna o documento com sucesso</response>
+    /// <response code="500">Se houver um erro interno no servidor</response>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public IActionResult Criar()
+    {
+        throw new ApplicationException("Método não concluido");
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="documentoId">Id do documento</param>
+    /// <response code="200">Retorna o documento com sucesso</response>
+    /// <response code="500">Se houver um erro interno no servidor</response>
+    [HttpPut("{documentoId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public IActionResult Alterar()
+    {
+        throw new ApplicationException("Método não concluido");
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="documentoId">Id do documento</param>
+    /// <response code="200">Retorna o documento com sucesso</response>
+    /// <response code="500">Se houver um erro interno no servidor</response>
+    [HttpDelete ("{documentoId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public IActionResult Deletar(int id)
+    {
+        throw new ApplicationException("Método não concluido");
+    }
+}
---
+++ Acessos/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Registre o gerador Swagger, definindo 1 ou mais documentos Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "AcessosAPI", Version = "v1" });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
