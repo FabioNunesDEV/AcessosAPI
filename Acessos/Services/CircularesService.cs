b9b489509b11598117d27fd2ba9d2a7931284f27
Fábio Nunes <fabionunesbr@gmail.com>
2024-07-24 07:32:12 -0300

    refactor: separando lógica

---
+++ Acessos/Services/CircularesService.cs
@@ -0,0 +1,33 @@
+﻿using Acessos.Data;
+using Acessos.DTO.Circular;
+using Acessos.Models;
+using AutoMapper;
+
+namespace Acessos.Services
+{
+    public class CircularesService
+    {
+        private readonly AcessoApiContext _context;
+        private readonly IMapper _mapper;
+
+        public CircularesService (AcessoApiContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public void CadastrarCircular(CircularCreateDTO dto)
+        {
+            var circular = _mapper.Map<Circular>(dto);
+
+            // Preenche campos automaticamente.
+            circular.Protocolo = Guid.NewGuid().ToString();
+            circular.DataEnvio = DateTime.Now;
+            circular.Status = "Pendente";
+
+            _context.Circulares.Add(circular);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetCircularPorId), new { id = circular.Id }, circular);
+        }
+    }
+}
